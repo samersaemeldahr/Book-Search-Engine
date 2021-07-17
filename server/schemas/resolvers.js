ce2d2c14717fc2cd91372300ae6334322e2f156d
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-16 23:41:36 -0500

    Add saveBook and removeBook mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -41,6 +41,30 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
+
+        saveBook: async (parent, { input }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Need to be logged in!')
+        },
+
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Need to be logged in!')
+        }
     }
 };
 
