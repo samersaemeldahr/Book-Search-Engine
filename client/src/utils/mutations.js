37d5ad06afd0fe3d835905b383be0e9a3eb09a7a
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-17 00:05:06 -0500

    Add login user and add user mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,41 @@
+import { gql } from '@apollo/client';
+
+// LOGIN_USER
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          title
+          description
+          authors
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+// ADD_USER
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// SAVE_BOOK
+
+// REMOVE_BOOK
