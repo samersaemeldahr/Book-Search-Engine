4920a295befd4b5af6e92d08507ca7a5562dd914
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-21 12:33:41 -0500

    Debug

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const db = require('./config/connection');
 
-const { ApolloServer } = require('apollo-server-express')
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -24,7 +24,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
