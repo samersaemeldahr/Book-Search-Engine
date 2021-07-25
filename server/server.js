2d192b529da481c159dd2cb3c1fae7b1b41a7da2
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-25 17:57:19 -0500

    Debugging

--- server/server.js
+++ server/server.js
@@ -20,13 +20,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
