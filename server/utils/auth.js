cae4b8c80d480588cebb172392417b6e08671d03
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-25 19:40:58 -0500

    Remove optional chaining

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,10 +8,10 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query?.token || req.headers?.authorization;
+    let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
-    if (req.headers?.authorization) {
+    if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
