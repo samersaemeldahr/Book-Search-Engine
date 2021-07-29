24a902334a730554999c092d0ed2cf15bd4f0865
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-29 16:36:08 -0500

    Updates

---
+++ precache-manifest.1a996c45724defc89737dd4f5c95a2c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8dd70f0d4c876c3b216a066c9cf70c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89df4c8acc09040c53ff",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "0d33bee4ea8abd5699f4",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "89df4c8acc09040c53ff",
+    "url": "/static/js/2.00f24a6e.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.00f24a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d33bee4ea8abd5699f4",
+    "url": "/static/js/main.ea058e9a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Book-Search-Engine/precache-manifest.5a69a215ecb57640d41126efd1129ede.js"
+  "/precache-manifest.1a996c45724defc89737dd4f5c95a2c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Book-Search-Engine/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
