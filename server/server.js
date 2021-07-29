c367e35244be0472661bea01b14526fbaf47aa09
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-29 16:02:25 -0500

    Ready to deploy

--- server/server.js
+++ server/server.js
@@ -16,17 +16,15 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'index.html'));
-// });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
