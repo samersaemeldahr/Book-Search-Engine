a528afd1177fe25fc24b528eedb35f4ed306aab8
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-29 16:01:45 -0500

    Updates

---
+++ precache-manifest.5a69a215ecb57640d41126efd1129ede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a44bfd4a976521a1bb5cb7d55b6de8",
+    "url": "/Book-Search-Engine/index.html"
+  },
+  {
+    "revision": "d0233771c7a06ff17a8c",
+    "url": "/Book-Search-Engine/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "b3d48407503945b69bfb",
+    "url": "/Book-Search-Engine/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "d0233771c7a06ff17a8c",
+    "url": "/Book-Search-Engine/static/js/2.bd54433a.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/Book-Search-Engine/static/js/2.bd54433a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3d48407503945b69bfb",
+    "url": "/Book-Search-Engine/static/js/main.2e7dad50.chunk.js"
+  },
+  {
+    "revision": "d97605c75cecf991a7a7",
+    "url": "/Book-Search-Engine/static/js/runtime-main.aa81579e.js"
+  }
+]);
